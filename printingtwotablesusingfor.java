2d4fe589f4fbad0c0285d699d4d66215af6587cb
admin <admin@localhost.localdomain>
2026-08-28 13:07:52 +0530

    Added a java program for printingtwotablesusingfor

---
+++ printingtwotablesusingfor.java
@@ -0,0 +1,7 @@
+public class printingtwotablesusingfor {
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++){
+        System.out.println(i+"x2="+i*2);
+        }
+    }    
+}
