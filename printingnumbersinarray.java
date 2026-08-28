158af604639df43eb8dc8ca0b719ca3ff4d40534
admin <admin@localhost.localdomain>
2026-08-28 13:01:37 +0530

    Added a java program for printingnumbersinarray

---
+++ printingnumbersinarray.java
@@ -0,0 +1,8 @@
+public class printingnumbersinarray {
+    public static void main(String[] args) {
+        int[] score = {1,2,3,4,5,6,7,8,9,10};
+        for (int i=0;i<=9;i++){
+            System.out.println(score[i]);
+        }
+    }
+}
