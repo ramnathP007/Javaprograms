dcfbcecc71d1ab8135523bdeff84b04cceee0182
admin <admin@localhost.localdomain>
2026-08-28 19:22:27 +0530

    Added two java programs for star pattern

---
+++ starpattern2.java
@@ -0,0 +1,23 @@
+//expected output
+/*
+
+*
+**
+***
+
+
+*/
+
+
+
+public class starpattern2 {
+    public static void main(String[] args) {
+        for (int j=1;j<=3;j++){
+            for (int i=1;i<=j;i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
