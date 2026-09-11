0c77e200106e0907be9fca986277d78e61c44c4a
Ramnath P <167675028+ramnathP007@users.noreply.github.com>
2026-09-11 18:42:06 +0530

    Created Addition.java

---
+++ Addition.java
@@ -0,0 +1,16 @@
+public class Addition {
+
+  int a = 10;
+  int b = 20;
+
+  void sum ()
+  {
+      System.out.println(a+b);
+  }
+
+  public static void main(String[] args)
+  {
+      Addition obj1 = new Addition();
+      obj1.sun();
+  }
+}
